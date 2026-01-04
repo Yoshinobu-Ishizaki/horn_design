a80af560eb78e5f8d6a424cfb18253137aced143
yoshinobu ishizaki <ysnbiszk@gmail.com>
2026-01-04 19:56:08 +0900

    b2 bow length calculated and model fixed

---
+++ slides/slides_length.py
@@ -0,0 +1,111 @@
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Slide length calculation
+
+    - F/Bb open lengths are assumed as below.
+    """)
+    return
+
+
+@app.cell
+def _():
+    bopen = 9 * 304.8 * 1.03 # 3% longer than 9 ft
+    fopen = 12 * 304.8 * 1.03 # 3% longer than 12 ft
+    return (bopen,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Also, each valve port length is assumed as,
+    """)
+    return
+
+
+@app.cell
+def _():
+    portlen = 35.0
+    return (portlen,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    Basic slide pipe length are measured (adding ferrule length and slide gap),
+    """)
+    return
+
+
+@app.cell
+def _():
+    slide_short = 36 + 6 + 1
+    slide_long = 67 + 6 + 1
+    return (slide_short,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Bb 2nd slide
+
+    half tone longer than Bb open.
+    """)
+    return
+
+
+@app.cell
+def _():
+    halftone_ratio = 2**(1/12)
+    return (halftone_ratio,)
+
+
+@app.cell
+def _(bopen, halftone_ratio):
+    b2_len = bopen * (halftone_ratio-1)
+    b2_len
+    return (b2_len,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    b2 bow length are calculated as,
+    """)
+    return
+
+
+@app.cell
+def _(b2_len, portlen, slide_short):
+    b2_bow = b2_len - portlen - 2 * slide_short
+    b2_bow
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+ 
+    """)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
